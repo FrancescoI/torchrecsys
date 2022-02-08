fdff5e46076de84a40862d265fbb1ce0d0b4c73e
Francesco Imbriglia <francesco.imbriglia01@gmail.com>
2022-02-08 12:16:03 +0100

    release v0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='torchrecsys',
-    version='v0.1.2',
+    version='v0.1.3',
     packages=find_packages(),
     install_requires=['torch', 'pandas', 'scipy', 'numpy', 'sklearn'],
     license='MIT',
@@ -10,7 +10,7 @@ setup(
     author='Francesco Imbriglia',
     author_email='francesco.imbriglia01@gmail.com',
     url='https://github.com/francescoi/torchrecsys',
-    download_url='https://github.com/FrancescoI/torchrecsys/archive/refs/tags/v0.1.2.tar.gz',
+    download_url='https://github.com/FrancescoI/torchrecsys/archive/refs/tags/v0.1.3.tar.gz',
     keywords=['recommendation', 'recommender', 'collaborative', 'filtering', 'sequence', 'model', 'pytorch'],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: MIT License',
